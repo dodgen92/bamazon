3a6b870126db6c67190ec5704dceef9dc978ac2f
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-05-12 02:56:03 -0400

    created mysql connection

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,3 +1,12 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer")
-var table = require("cli-table2")
+var inquirer = require("inquirer");
+var table = require("cli-table2");
+
+var connection = mysql.createConnection({
+    host:"",
+    user:"",
+    password:"",
+    database:"",
+    port:3000
+})
+connection.connect;
