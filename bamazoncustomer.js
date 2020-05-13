f0845a3cb463189c24e15cc40fa9451d2b4f0250
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-05-13 02:14:32 -0400

    added display function

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,12 +1,28 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var table = require("cli-table2");
+var Table = require("cli-table2");
 
 var connection = mysql.createConnection({
     host:"",
     user:"",
     password:"",
     database:"",
-    port:3000
+    port:3306
 })
 connection.connect;
+
+
+// loads data from products table to terminal console
+function display() {
+    // Selects all of the data from the MySQL products table
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log("Welcome to Bamazon!")
+      // Lists table into the console
+      console.table(res);
+     
+      promptCustomerForItem(res);
+    });
+  };
+  
+  
